4c70cea82e389dba18d39762e2e33893b7b99f5f
Andrei Lialenko <andreilialenko@mail.ru>
2023-05-11 14:07:01 +0300

    python

---
+++ main.py
@@ -0,0 +1,156 @@
+# print('Результат:',7,15, sep='',end='!\n')
+# print('Second \' \t \\ L\ni\nn\ne')
+# print('Результат:',5//3)
+# print('Результат:',round(5/3))
+
+# input('Введите свой возраст: ')
+# number = 5 
+
+# digit = -4.5555556
+# word = 'Резутьтат:'
+# boolean = False
+
+# str_num='5'
+
+# # print(word+ str (digit))
+# print(word+ str(number+ int(str_num)))
+
+# del number
+
+# number = 7
+# print('Резутьтат:',number)
+
+# num1= int(input('Ведите первое число: '))
+
+# num2= int(input('Ведите первое число: '))
+
+# num1*=5
+
+# print('result:',num1+num2)
+# print('result:',num1-num2)
+# print('result:',num1/num2)
+# print('result:',num1*num2)
+# print('result:',num1**num2)
+# print('result:',num1//num2)
+
+# word="hi"
+# print(word*2)
+
+# word=True
+
+# user_data = int(input('Введите число: '))
+
+# isHappy= True
+
+# if  isHappy or user_data==6:
+#     print(' Пользователь Счастлив')
+# elif user_data==5:
+#     print('Заявлено 5')
+# elif user_data==7:
+#     print('Заявлено 7')
+# else:
+#     print('Пользователь не счастлив')
+#  if user_data!=5:
+#      print('Мы на месте')
+#     if user_data>6:
+#          print('Число больше чем 5')
+       
+# data = input()
+# number=5 if data == 'Five' else 0
+# if data =='Five':
+#     number = 5
+# else:
+#     number =0
+# print(number)
+
+# for i in range(1,15,4):
+#     print(i)
+# count=0
+# word = 'Привет!'
+# for i in word:
+#     if i == 'т':
+#         count +=1
+# print('Count:',count)
+
+# i=5 
+# isHasCar=True
+
+# while isHasCar:
+#     if input('Ввидите данные: ')=='Стоп':
+#         isHasCar=False
+  
+# for i in range(1,11):
+#     if i ==5:
+#         break
+
+#     if i%2==0:
+#         continue
+
+#     print(i)
+
+# for i in 'привет':
+#     if i =='в':
+#         fount= True
+#         break
+#     else:
+#         fount=False
+# print(fount)
+
+# nums = [5,6,8,9,-5,5.2,True,'Hello',[5,1,2,3,6,4]]
+# nums[0]=125
+# nums[3]=-56
+# nums[6]=False
+# print(nums[-1][1])
+
+# numbers = [100,250,253]
+# # numbers[3]=100 #нельзя так добовлять
+# numbers.append(100)
+# numbers.append(True)
+# numbers.insert(1,100)
+
+# d=[550,660,850,900,3]
+# numbers.extend(d)
+# # numbers.reverse()
+# numbers.sort()
+# numbers.pop(-5)
+# numbers.remove(3)
+
+# # numbers.clear()
+
+# print(len(numbers))
+
+# nums = [5,2,7,'50',False]
+
+# for el in nums:
+#     el *=2
+#     print(el)
+
+# n = int(input('Введите длину списка: '))
+# user_list=[]
+# i = 0
+
+# while i<n:
+#     string = 'Ввидите элемент #'+ str (i+1)+':'
+#     user_list.append(input(string))
+#     i+=1
+# print(user_list)
+
+word = 'футболл, баскетболл, валейбол, skate'
+# print(len(word))
+# print(word.lower())
+# print(word.upper())
+# print(word.isupper())
+# print(word.lower())
+# print(word.islower())
+# print(word.count('l'))
+# print(word.capitalize())
+# print(word.find('L'))
+# print(word.split(', '))
+# hobby = word.split(', ')
+
+# for i in range(len(hobby)) :
+#     hobby[i]= hobby[i].capitalize()
+
+# result = '|'.join(hobby) 
+# print(result)   
+
